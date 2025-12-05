a87e5bc3cf5138eb1e953f57709c01a23370efb7
Bifbof <christof.leutenegger@hotmail.com>
2025-12-05 16:12:29 +0100

    make day 5 a bit simpler

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,23 +1,8 @@
 fn main() {
     let (ranges, mut numbers) = parse();
     let ranges = merge_ranges(ranges);
-    numbers.sort_unstable();
-    let answer1 = part1(&ranges, numbers);
-    let answer2 = part2(&ranges);
-    println!("{answer1}");
-    println!("{answer2}");
-}
-
-fn merge_ranges(mut ranges: Vec<(u64, u64)>) -> Vec<(u64, u64)> {
-    ranges.sort_unstable();
-    let mut merged = Vec::new();
-    for (from, to) in ranges {
-        match merged.last_mut() {
-            Some((_, prev_to)) if from <= *prev_to => *prev_to = to.max(*prev_to),
-            _ => merged.push((from, to)),
-        }
-    }
-    merged
+    part1(&ranges, &mut numbers);
+    part2(&ranges);
 }
 
 fn parse() -> (Vec<(u64, u64)>, Vec<u64>) {
@@ -36,26 +21,39 @@ fn parse() -> (Vec<(u64, u64)>, Vec<u64>) {
     (ranges, numbers)
 }
 
-fn part1(ranges: &[(u64, u64)], numbers: Vec<u64>) -> u64 {
+fn merge_ranges(mut ranges: Vec<(u64, u64)>) -> Vec<(u64, u64)> {
+    ranges.sort_unstable();
+    let mut merged = Vec::new();
+    for (from, to) in ranges {
+        match merged.last_mut() {
+            Some((_, prev_to)) if from <= *prev_to => *prev_to = to.max(*prev_to),
+            _ => merged.push((from, to)),
+        }
+    }
+    merged
+}
+
+fn part1(ranges: &[(u64, u64)], numbers: &mut [u64]) {
     let mut ranges = ranges.iter().peekable();
+    numbers.sort_unstable();
     let mut good = 0;
-    for number in numbers {
-        let (from, to) = loop {
-            match ranges.peek() {
-                None => return good,
-                Some((_, to)) if *to < number => {
-                    ranges.next();
-                }
-                Some((from, to)) => break (from, to),
-            }
+    for number in numbers.iter() {
+        while let Some((_, to)) = ranges.peek()
+            && to < number
+        {
+            ranges.next();
+        }
+        let Some((from, to)) = ranges.peek() else {
+            break;
         };
-        if *from <= number && number <= *to {
+        if (from..=to).contains(&number) {
             good += 1;
         }
     }
-    good
+    println!("{good}");
 }
 
-fn part2(ranges: &[(u64, u64)]) -> u64 {
-    ranges.iter().map(|(from, to)| to - from + 1).sum()
+fn part2(ranges: &[(u64, u64)]) {
+    let count: u64 = ranges.iter().map(|(from, to)| to - from + 1).sum();
+    println!("{count}");
 }
