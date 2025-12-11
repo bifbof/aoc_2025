90cd4c29886be6216c52c0af80a23ccf498a6cc0
Bifbof <christof.leutenegger@hotmail.com>
2025-12-11 22:44:55 +0100

    day 11 part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -1,144 +1,91 @@
-use std::{cmp::Reverse, collections::HashMap, fmt::Debug, str::FromStr};
+use std::{cmp::Reverse, collections::{HashMap, HashSet}};
 
-use itertools::Itertools;
-
-#[derive(PartialEq, Eq, Hash, Clone, Copy)]
-struct NodeId(u16);
-
-impl Debug for NodeId {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let rest = self.0;
-        let c = u8::try_from(rest % 26).unwrap() + b'a';
-        let rest = rest / 26;
-        let b = u8::try_from(rest % 26).unwrap() + b'a';
-        let rest = rest / 26;
-        let a = u8::try_from(rest).unwrap() + b'a';
-        let bytes = [a, b, c];
-        let s = str::from_utf8(&bytes).unwrap();
-        f.write_str(s)
-    }
-}
-
-impl FromStr for NodeId {
-    type Err = ();
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let s = s.as_bytes();
-        if let [a, b, c] = s
-            && a.is_ascii_lowercase()
-            && b.is_ascii_lowercase()
-            && c.is_ascii_lowercase()
-        {
-            let a: u16 = (a - b'a').into();
-            let b: u16 = (b - b'a').into();
-            let c: u16 = (c - b'a').into();
-            Ok(Self(((a * 26) + b) * 26 + c))
-        } else {
-            Err(())
-        }
-    }
-}
-
+// for some unnecessary complications reason lets make everything &str
 #[derive(Debug)]
-struct Graph {
-    edges: HashMap<NodeId, HashMap<NodeId, u64>>,
+struct Graph<'graph> {
+    edges: HashMap<&'graph str, Vec<&'graph str>>,
 }
 
-impl Graph {
+impl<'graph> Graph<'graph> {
     fn new() -> Self {
         let edges = HashMap::new();
         Self { edges }
     }
 
-    fn add_edge(&mut self, from: &str, to: &str) {
-        // super bad idea in production code ;)
-        // but for us it makes computing the flow easier
-        if to == "you" {
-            return;
-        }
-        let from = from.parse().unwrap();
-        let to = to.parse().unwrap();
+    fn add_edge(&mut self, from: &'graph str, to: &'graph str) {
         // build the residual graph directly
-        self.edges.entry(from).or_default().insert(to, 1);
-        self.edges.entry(to).or_default().insert(from, 0);
+        self.edges.entry(from).or_default().push(to);
     }
 
-    fn compute_flow(&mut self) -> u64 {
-        let from: NodeId = "you".parse().unwrap();
-        let to: NodeId = "out".parse().unwrap();
-        loop {
-            // find a path with remaining capacity
-            let mut stack = vec![from];
-            let mut visited = HashMap::from([(from, from)]);
-            while let Some(node) = stack.pop() {
-                for (neigh, _) in self.edges[&node].iter().filter(|(_, c)| **c > 0) {
-                    if !visited.contains_key(neigh) {
-                        visited.insert(*neigh, node);
-                        stack.push(*neigh);
-                    }
-                }
-            }
-            if !visited.contains_key(&to) {
-                break;
-            }
-            let mut path = vec![to];
-            let mut node = to;
-            while node != from {
-                node = visited[&node];
-                path.push(node);
-            }
-            println!("path {path:?}");
-            for (u, v) in path.iter().tuple_windows() {
-                println!("{u:?}, {v:?}");
-                *self.edges.get_mut(u).unwrap().get_mut(v).unwrap() += 1;
-                *self.edges.get_mut(v).unwrap().get_mut(u).unwrap() -= 1;
-            }
-        }
-        self.edges[&from].values().map(|v| 1 - v).sum()
-    }
-
-    fn dfs_exit_times(&self, node: NodeId, time: &mut u64, outtime: &mut HashMap<NodeId, u64>) {
-        println!("{:?}", node);
+    fn dfs_exit_times(
+        &self,
+        node: &'graph str,
+        time: &mut u64,
+        outtime: &mut HashMap<&'graph str, u64>,
+    ) {
         if outtime.contains_key(&node) {
             return;
         }
-        println!("{:?}", self.edges[&node]);
-        for (&neigh, _) in self.edges[&node].iter().filter(|(_, v)| **v > 0) {
-            self.dfs_exit_times(neigh, time, outtime);
+        if let Some(neighs) = self.edges.get(node) {
+            for neigh in neighs {
+                self.dfs_exit_times(neigh, time, outtime);
+            }
         }
         outtime.insert(node, *time);
         *time += 1;
     }
 
-    fn part1(&self) -> u64 {
-        let you: NodeId = "you".parse().unwrap();
-        let out: NodeId = "out".parse().unwrap();
+    fn solve(&self, from: &str, to: &str) -> (u64, u64) {
         let mut map = HashMap::new();
-        self.dfs_exit_times(you, &mut 0, &mut map);
+        self.dfs_exit_times(from, &mut 0, &mut map);
         let mut times: Vec<_> = map.into_iter().collect();
         times.sort_unstable_by_key(|&(_, time)| Reverse(time));
-        let mut possibilities = HashMap::from([(you, 1)]);
+        // {visited neither, visited fft, visited dac, visited both}
+        let mut possibilities = HashMap::from([(&from, (1, 0, 0, 0))]);
         for (node, _) in times {
-            for (&neigh, _) in self.edges[&node].iter().filter(|(_, v)| **v > 0) {
-                *possibilities.entry(neigh).or_insert(0) += possibilities[&node];
+            let Some(neighs) = self.edges.get(node) else {
+                continue;
+            };
+            for neigh in neighs {
+                let pnode = possibilities[&node];
+                let pneigh = possibilities.entry(neigh).or_insert((0, 0, 0, 0));
+                match *neigh {
+                    "fft" => {
+                        pneigh.1 += pnode.0;
+                        pneigh.3 += pnode.2;
+                    }
+                    "dac" => {
+                        pneigh.2 += pnode.0;
+                        pneigh.3 += pnode.1;
+                    }
+                    _ => {
+                        pneigh.0 += pnode.0;
+                        pneigh.1 += pnode.1;
+                        pneigh.2 += pnode.2;
+                        pneigh.3 += pnode.3;
+                    }
+                }
             }
         }
-        possibilities[&out]
+        let out = possibilities[&to];
+        let part1 = out.0 + out.1 + out.2 + out.3;
+        let part2 = out.3;
+        (part1, part2)
     }
 }
 
 fn main() {
-    let mut graph = parse();
-    println!("{}", graph.part1());
-}
-
-fn parse() -> Graph {
     let data = std::fs::read_to_string("input/11.txt").unwrap();
-    let mut graph = Graph::new();
-    for line in data.lines() {
-        let (from, tos) = line.split_once(": ").unwrap();
-        for to in tos.split_ascii_whitespace() {
-            graph.add_edge(from, to);
+    let graph = {
+        let mut graph = Graph::new();
+        for line in data.lines() {
+            let (from, tos) = line.split_once(": ").unwrap();
+            for to in tos.split_ascii_whitespace() {
+                graph.add_edge(from, to);
+            }
         }
-    }
-    graph
+        graph
+    };
+    println!("{}", graph.solve("you", "out").0);
+    println!("{}", graph.solve("svr", "out").1);
 }
